a00d3e7840a0343f6696c56f076b3695eda8ae49
Yogie Yadav <yogiejay21@gmail.com>
2024-01-21 02:31:46 +0530

    added config to firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC4fDilOuf5Ux7CbmZeDeRctr4sr9GSxe0",
+  authDomain: "challange-418c1.firebaseapp.com",
+  projectId: "challange-418c1",
+  storageBucket: "challange-418c1.appspot.com",
+  messagingSenderId: "174089061963",
+  appId: "1:174089061963:web:b4ded06ca0748d40e5afce",
+  measurementId: "G-XZS6MB9K59",
+};
